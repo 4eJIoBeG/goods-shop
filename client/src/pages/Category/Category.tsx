ec51b16dedd085f21e5eeea0ad4ef5df54357759
4eJIoBeG <pavelshashenko@gmail.com>
2024-03-30 20:27:37 +0300

    add items in category, need fixes

--- client/src/pages/Category/Category.tsx
+++ client/src/pages/Category/Category.tsx
@@ -4,6 +4,31 @@ import { Category } from "./Category.props";
 
 const Category = () => {
   const data = useLoaderData() as Category;
+  const [items, setItems] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | undefined>();
+
+  const getItems = async () => {
+    const params = {
+      page: 1,
+      limit: 50,
+      category: categoryId,
+    };
+    try {
+      const { data } = await axios.get<ApiResponse>(
+        `${BASE_URL_API}/item?limit=${params.limit}&page=${params.page}&category=${params.category}`,
+      );
+
+      setItems(data.rows);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.message);
+      }
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   console.log(data);
 
   return (
