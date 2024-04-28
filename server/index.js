a25cf71dc0b03689f237d2cfbb1a3048a5d9c1bc
4eJIoBeG <pavelshashenko@gmail.com>
2024-04-28 14:35:20 +0300

    prepeare files and fix some bugs before test deploy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:4173"],
     credentials: true,
     exposedHeaders: "set-cookie",
   }),
